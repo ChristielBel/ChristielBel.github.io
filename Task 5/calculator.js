df3e0919fab09ea91ed2481b049c2725205b0a4c
Christina Belikova <119342375+ChristielBel@users.noreply.github.com>
2023-10-12 08:06:47 +0300

    Update calculator.js

--- Task 5/calculator.js
+++ Task 5/calculator.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener("DOMContentLoaded", function() {
     var calculateButton = document.getElementById("calculateButton");
     calculateButton.addEventListener("click", function(event) {
         event.preventDefault();
