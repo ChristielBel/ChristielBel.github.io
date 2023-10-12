b3333d496755f26f22eec761d887ffbb2613cfba
Christina Belikova <119342375+ChristielBel@users.noreply.github.com>
2023-10-12 08:58:57 +0300

    Update calculator.js

--- Task 5/calculator.js
+++ Task 5/calculator.js
@@ -1,18 +1,19 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var calculateButton = document.getElementById("calculateButton");
+    let calculateButton = document.getElementById("calculateButton");
     calculateButton.addEventListener("click", function(event) {
         event.preventDefault();
-        var quantityInput = document.getElementById("quantity");
-        var productSelect = document.getElementById("product");
-        var resultDiv = document.getElementById("result");
+        let quantityInput = document.getElementById("quantity");
+        let productSelect = document.getElementById("product");
+        let resultDiv = document.getElementById("result");
 
-        var quantity = parseInt(quantityInput.value);
-        var price = parseInt(productSelect.value);
-
-        if (isNaN(quantity) || isNaN(price)) {
+        let str1 = quantityInput.value;
+        let str2 = productSelect.value;
+        if (str1.match(/^\d+$/) === null || str2.match(/^\d+$/) === null) {
             resultDiv.innerHTML = "Ошибка: введите корректные данные";
         } else {
-            var total = quantity * price;
+            let quantity = parseInt(quantityInput.value);
+            let price = parseInt(productSelect.value);
+            let total = quantity * price;
             resultDiv.innerHTML = "Стоимость заказа: ₽" + total;
         }
     });
