48b34554c0deece239578d645b3f356b3e6487c1
Christina Belikova <119342375+ChristielBel@users.noreply.github.com>
2023-10-22 20:34:42 +0300

    Create gallery.js

---
+++ Task 7/gallery.js
@@ -0,0 +1,31 @@
+/*global document */
+/*global $ */
+
+$(document).ready(function () {
+    $(".gallery").slick({
+        dots: true,
+        mobileFirst: true,
+        responsive: [
+            {
+                breakpoint: 1000,
+                settings: {
+                    slidesToScroll: 1,
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 400,
+                settings: {
+                    slidesToScroll: 1,
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+    $(document).ready(function () {
+        var scrollToFirstPageButton = $("#scrollToFirstPage");
+        scrollToFirstPageButton.click(function () {
+            $(".gallery").slick("slickGoTo", 0);
+        });
+    });
+});
