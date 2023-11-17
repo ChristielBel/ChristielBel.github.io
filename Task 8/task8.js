e94cb76af2678951e7d2073535e2de36227bb18e
Christina Belikova <119342375+ChristielBel@users.noreply.github.com>
2023-11-17 22:51:24 +0300

    Update task8.js

--- Task 8/task8.js
+++ Task 8/task8.js
@@ -1,57 +1,74 @@
-document.getElementById('openFormButton').addEventListener('click', function() {
-    document.getElementById('feedbackFormPopup').style.display = 'block';
-    history.pushState({ formOpen: true }, '', '/feedback-form');
-  });
-  
-  window.addEventListener('popstate', function(event) {
-    if (event.state && event.state.formOpen) {
-      document.getElementById('feedbackFormPopup').style.display = 'block';
-    } else {
-      document.getElementById('feedbackFormPopup').style.display = 'none';
+document.addEventListener('DOMContentLoaded', function () {
+    const formWrapper = document.getElementById('feedbackFormWrapper');
+    const openFormBtn = document.getElementById('openFormBtn');
+    const closeFormBtn = document.getElementById('closeFormBtn');
+    const feedbackForm = document.getElementById('feedbackForm');
+    const feedbackMessage = document.getElementById('feedbackMessage');
+
+    openFormBtn.addEventListener('click', function () {
+        formWrapper.style.display = 'flex';
+        history.pushState({ formOpen: true }, null, '?formOpen=true');
+    });
+
+    closeFormBtn.addEventListener('click', function () {
+        formWrapper.style.display = 'none';
+        history.pushState({ formOpen: false }, null, '?formOpen=false');
+    });
+
+    window.addEventListener('popstate', function (event) {
+        if (event.state && event.state.formOpen) {
+            formWrapper.style.display = 'flex';
+        } else {
+            formWrapper.style.display = 'none';
+        }
+    });
+
+    feedbackForm.addEventListener('input', function () {
+        localStorage.setItem('feedbackFormData', JSON.stringify({
+            fullName: document.getElementById('fullName').value,
+            email: document.getElementById('email').value,
+            phone: document.getElementById('phone').value,
+            organization: document.getElementById('organization').value,
+            message: document.getElementById('message').value,
+            agreement: document.getElementById('agreement').checked,
+        }));
+    });
+
+    feedbackForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        const formData = new FormData(feedbackForm);
+
+        const xhr = new XMLHttpRequest();
+        xhr.open('POST', 'https://formcarry.com/s/N_nhDhYffl');
+        xhr.setRequestHeader('Accept', 'application/json');
+
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === XMLHttpRequest.DONE) {
+                if (xhr.status === 200) {
+                    feedbackMessage.textContent = 'Форма успешно отправлена!';
+                } else {
+                    feedbackMessage.textContent = 'Произошла ошибка при отправке формы. Попробуйте еще раз.';
+                }
+
+                feedbackForm.reset();
+
+                setTimeout(function () {
+                    formWrapper.style.display = 'none';
+                }, 3000);
+            }
+        };
+
+        xhr.send(formData);
+    });
+
+    const savedFormData = JSON.parse(localStorage.getItem('feedbackFormData'));
+    if (savedFormData) {
+        document.getElementById('fullName').value = savedFormData.fullName;
+        document.getElementById('email').value = savedFormData.email;
+        document.getElementById('phone').value = savedFormData.phone;
+        document.getElementById('organization').value = savedFormData.organization;
+        document.getElementById('message').value = savedFormData.message;
+        document.getElementById('agreement').checked = savedFormData.agreement;
     }
-  });
-  
-  document.getElementById('feedbackForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-  
-    const formData = new FormData(this);
-    // Отправка данных на сервер с помощью XHR или Axios
-    // Пример с использованием Axios:
-    axios.post('https://formcarry.com/s/ВАШ_ID_ФОРМЫ', formData)
-      .then(function(response) {
-        document.getElementById('feedbackMessage').innerText = 'Сообщение успешно отправлено';
-        document.getElementById('feedbackMessage').style.display = 'block';
-        // Очистка формы
-        document.getElementById('feedbackForm').reset();
-      })
-      .catch(function(error) {
-        document.getElementById('feedbackMessage').innerText = 'Ошибка при отправке сообщения';
-        document.getElementById('feedbackMessage').style.display = 'block';
-      });
-  });
-  
-  // Восстановление последних введенных значений из LocalStorage
-  document.addEventListener('DOMContentLoaded', function() {
-    const lastFormData = JSON.parse(localStorage.getItem('lastFormData'));
-    if (lastFormData) {
-      document.getElementById('fullName').value = lastFormData.fullName;
-      document.getElementById('email').value = lastFormData.email;
-      document.getElementById('phone').value = lastFormData.phone;
-      document.getElementById('organization').value = lastFormData.organization;
-      document.getElementById('message').value = lastFormData.message;
-      document.getElementById('agreement').checked = lastFormData.agreement;
-    }
-  });
-  
-  // Сохранение последних введенных значений в LocalStorage перед закрытием страницы
-  window.addEventListener('beforeunload', function() {
-    const formData = {
-      fullName: document.getElementById('fullName').value,
-      email: document.getElementById('email').value,
-      phone: document.getElementById('phone').value,
-      organization: document.getElementById('organization').value,
-      message: document.getElementById('message').value,
-      agreement: document.getElementById('agreement').checked
-    };
-    localStorage.setItem('lastFormData', JSON.stringify(formData));
-  });
+});
