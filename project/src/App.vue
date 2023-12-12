25cdc2eec6ae148b40098e2710a81c73c8a4239a
Christina Belikova <kristina04bel@gmail.com>
2023-12-12 17:00:45 +0300

    Added blocks about task completion time and services

--- project/src/App.vue
+++ project/src/App.vue
@@ -1,12 +1,16 @@
 <script setup>
 import Header from "@/components/Header.vue";
 import Support from "@/components/Support.vue";
+import Service from "@/components/Service.vue"
+import TimeTasks from "@/components/TimeTasks.vue";
 </script>
 
 <template>
   <div class="main">
     <Header/>
+    <Service/>
     <Support/>
+    <TimeTasks/>
   </div>
 </template>
 
